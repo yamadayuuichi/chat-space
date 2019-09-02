7b72f224f1a6730adb54923a45fc0df6007928a8
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-09-02 15:16:34 +0900

    add test

--- Gemfile
+++ Gemfile
@@ -60,3 +60,12 @@ gem 'devise'
 
 gem 'carrierwave'
 gem 'mini_magick'
+
+group :development, :test do
+  gem 'byebug', platform: :mri
+  gem 'pry-rails'
+  gem 'rspec-rails', '~> 3.5'
+  gem 'rails-controller-testing'
+  gem 'factory_bot_rails'
+  gem 'faker'
+end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,14 +15,18 @@ class GroupsController < ApplicationController
     else
       render :new
     end
+  end
 
-    def update
-      if @group.update(group_params)
-        redirect_to group_messages_path(@group), notice: 'グループを編集しました'
-      else
-        render :edit
-      end
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
     end
+  end
+  
+  def edit
+    
   end
 
   private
