79fd33c8d4ac214d978fa4098998d052885f7709
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-09-02 16:28:31 +0900

    retouch　users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if currenty_user.update(user_params)
+    if current_user.update(user_params)
       redirect_to root_path
     else
       render :edit
