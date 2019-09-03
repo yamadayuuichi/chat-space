d8dbb059b52698f791d91babdd6bc0980da2b48d
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-09-03 19:48:22 +0900

    indicate group member

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,6 +17,9 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
@@ -25,10 +28,6 @@ class GroupsController < ApplicationController
     end
   end
   
-  def edit
-    
-  end
-
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
