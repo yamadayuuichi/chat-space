7b7ddd176a2f11b7b036617974de53442c881913
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-09-05 02:46:27 +0900

    plus Incremental search

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -16,6 +16,18 @@ $(document).on('turbolinks:load', function() {
       search_list.append(html);
     }
 
+    var member_list = $("#chat-group-users");
+
+    function addUser(userId,userName) {
+    var html = `<div id='chat-group-users'>
+                  <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                    <input name='group[user_ids][]' type='hidden' value='${userId}'>
+                      <p class='chat-group-user__name'>${userName}</p>
+                      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    member_list.append(html);
+      }
+
     $("#user-search-field").on("keyup", function(){
       var input = $("#user-search-field").val();
       
@@ -39,6 +51,17 @@ $(document).on('turbolinks:load', function() {
       .fail(function(){
         alert('検索に失敗しました');
       })
-    })
+    });
+      $(document).on("click", ".user-search-add", function () {
+        $('#chat-group-users').val();
+          var userId = $(this).data('user-id');
+          var userName = $(this).data('user-name');
+          addUser(userId,userName);
+          $(this).parent().remove();
+      });
+  
+      $(document).on("click", ".user-search-remove", function () {
+        $(this).parent().remove();
+      });
   });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update, :index]
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, only: [:new, :create, :edit, :update, :index] do
     resources :messages, only: [:index, :create, :edit]
   end
 end
