8bc1eaafc9ce5019a38f413316469673b83d526c
yamadayuuichi <yuichi.xuxu@gmail.com>
2019-09-05 00:23:26 +0900

    add Incremental search to usercontroller action index

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,46 +1,48 @@
-$(function(){
+$(document).on('turbolinks:load', function() {
+  $(function(){
 
-  function buildHTML(message){
-    var img = message.image ? `<img src= ${ message.image }>` : "";
-    var html = `<div class="message">
-                <div class="upper-message">
-                <div class="upper-message__user-name">
-                  ${message.user_name}
-                </div>
-                <div class="upper-message__date">
-                  ${message.date}
-                </div>
-                </div>
-                <div class="lower-message">
-                <p class="lower-message__content">
-                  ${message.content}
-                </p>
-                  ${img}
-                </div>
-                </div>`
-    return html;
-  }
+    function buildHTML(message){
+      var img = message.image ? `<img src= ${ message.image }>` : "";
+      var html = `<div class="message">
+                  <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.date}
+                  </div>
+                  </div>
+                  <div class="lower-message">
+                  <p class="lower-message__content">
+                    ${message.content}
+                  </p>
+                    ${img}
+                  </div>
+                  </div>`
+      return html;
+    }
 
-  $("#new_message").on("submit", function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = (window.location.href);
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: "json",
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html).animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('input').prop('disabled', false);
-      $('.new_message')[0].reset();
-    })
-    .fail(function(){
-      alert('error')
-    })
+    $("#new_message").on("submit", function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = (window.location.href);
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: "json",
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.messages').append(html).animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        $('input').prop('disabled', false);
+        $('.new_message')[0].reset();
+      })
+      .fail(function(){
+        alert('error')
+      })
+    });
   });
 });
